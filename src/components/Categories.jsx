60fcf0f2d330cd89e79ab0d28426579c56833225
Baklay Petro <pustota1988@gmail.com>
2023-02-11 20:11:57 +0200

    createMainComponents

---
+++ src/components/Categories.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Categories = () => {
+    return (
+        <div className="categories">
+            <ul>
+                <li className="active">Все</li>
+                <li>Iphone</li>
+                <li>Sumsung</li>
+                <li>Lenovo</li>
+                <li>LG</li>
+                <li>Xiomi</li>
+            </ul>
+        </div>
+    );
+};
+
+export default Categories;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-import './scss/app.scss';
+import App from "./App";
 
-import App from './App';
+import './scss/app.scss'
 
-ReactDOM.render(
-  <React.StrictMode>
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
+  </StrictMode>
 );
