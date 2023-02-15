b7a0708d35811ff3146efaa7f0063ea7277aa549
Baklay Petro <pustota1988@gmail.com>
2023-02-15 21:43:20 +0200

    createSckeleton

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Categories from './components/Categories';
 import Header from './components/Header';
 import ItemBlock from './components/ItemBlock';
+import Skeleton from './components/ItemBlock/Skeleton';
 import Sort from './components/Sort';
 
-import devices from './assets/devaices.json';
-
 function App() {
+    const [items, setItems] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        fetch('https://63ed0caae6ee53bbf5901b77.mockapi.io/devices')
+            .then((res) => res.json())
+            .then((devices) => {
+                setItems(devices);
+                setIsLoading(false)
+            });
+    }, []);
+
     return (
         <div className="wrapper">
             <Header />
@@ -18,9 +29,9 @@ function App() {
                     </div>
                     <h2 className="content__title">Все Девайсы</h2>
                     <div className="content__items">
-                        {devices.map((item) => (
-                            <ItemBlock {...item} key={item.id} />
-                        ))}
+                        {isLoading
+                            ? [...new Array(6).keys()].map((i) => <Skeleton key={i} />)
+                            : items.map((item) => <ItemBlock {...item} key={item.id} />)}
                     </div>
                 </div>
             </div>
---
+++ src/components/ItemBlock/Skeleton.jsx
@@ -0,0 +1,22 @@
+import React from "react"
+import ContentLoader from "react-content-loader"
+
+const Skeleton = (props) => (
+  <ContentLoader 
+    speed={2}
+    width={280}
+    height={500}
+    viewBox="0 0 280 470"
+    backgroundColor="#c7c7c7"
+    foregroundColor="#b5b5b5"
+    {...props}
+  >
+    <rect x="25" y="0" rx="20" ry="20" width="206" height="260" /> 
+    <rect x="25" y="266" rx="5" ry="5" width="206" height="40" /> 
+    <rect x="0" y="316" rx="5" ry="5" width="260" height="88" /> 
+    <rect x="0" y="423" rx="5" ry="5" width="95" height="27" /> 
+    <rect x="135" y="415" rx="5" ry="5" width="124" height="41" />
+  </ContentLoader>
+)
+
+export default Skeleton
