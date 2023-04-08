95200590431eaa25042d6474950fdedb4932e803
Baklay Petro <pustota1988@gmail.com>
2023-04-08 13:36:05 +0300

    Outlet

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
 import Home from './Pages/Home';
 import Cart from './Pages/Cart';
 import NotFound from './Pages/NotFound';
 import FullDevice from './Pages/FullDevice';
+import MainLayouot from './layouts/MainLayouot';
 
 function App() {
-
     return (
-        <div className="wrapper">
-                <Header />
-                <div className="content">
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/store-react" element={<Home />} />
-                        <Route path="/cart" element={<Cart />} />
-                        <Route path="*" element={<NotFound />} />
-
-                        <Route path="/device/:id" element={<FullDevice />} />
-                    </Routes>
-                </div>
-        </div>
+        <Routes>
+            <Route path="/" element={<MainLayouot />}>
+                <Route path="" element={<Home />} />
+                <Route path="store-react" element={<Home />} />
+                <Route path="cart" element={<Cart />} />
+                <Route path="device/:id" element={<FullDevice />} />
+                <Route path="*" element={<NotFound />} />
+            </Route>
+        </Routes>
     );
 }
 
