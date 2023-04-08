93c7acd6b2783b073b2a02e4c6f119af32c304a3
Baklay Petro <pustota1988@gmail.com>
2023-04-08 12:44:08 +0300

    createFullPage

--- src/components/ItemBlock/index.jsx
+++ src/components/ItemBlock/index.jsx
@@ -1,18 +1,18 @@
 import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux'
-import {addItem, cartItemSelector} from '../../redux/slices/cartSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { addItem, cartItemSelector } from '../../redux/slices/cartSlice';
+import { Link } from 'react-router-dom';
 
-const ItemBlock = ({ id ,title, price, imageUrl, sizes, types }) => {
-    const dispatch = useDispatch()
-    const cartItem = useSelector(cartItemSelector(id))
+const ItemBlock = ({ id, title, price, imageUrl, sizes, types }) => {
+    const dispatch = useDispatch();
+    const cartItem = useSelector(cartItemSelector(id));
 
-    const addedCount = cartItem ? cartItem.count : 0
-    
-    const typeName = ['128GB', '256GB']
+    const addedCount = cartItem ? cartItem.count : 0;
+
+    const typeName = ['128GB', '256GB'];
 
     const [activeSize, setActiveSize] = useState(0);
     const [activeType, setActiveType] = useState(0);
-
 
     const onClickAdd = () => {
         const item = {
@@ -22,25 +22,41 @@ const ItemBlock = ({ id ,title, price, imageUrl, sizes, types }) => {
             imageUrl,
             type: typeName[activeType],
             size: sizes[activeSize],
-        }
-        
-        dispatch(addItem(item))
+        };
+
+        dispatch(addItem(item));
     };
 
     return (
         <div className="pizza-block">
-            <img className="pizza-block__image" src={imageUrl} alt="Phone" />
-            <h4 className="pizza-block__title">{title}</h4>
+            <Link to={`/device/${id}`}>
+                <img className="pizza-block__image" src={imageUrl} alt="Phone" />
+                <h4 className="pizza-block__title">{title}</h4>{' '}
+            </Link>
+
             <div className="pizza-block__selector">
                 <ul>
-                    
                     {types.map((typeId) => {
-                        return <li onClick={() => setActiveType(typeId)} className={activeType === typeId ? "active" : ""} key={typeId}>{typeName[typeId]}</li>
+                        return (
+                            <li
+                                onClick={() => setActiveType(typeId)}
+                                className={activeType === typeId ? 'active' : ''}
+                                key={typeId}
+                            >
+                                {typeName[typeId]}
+                            </li>
+                        );
                     })}
                 </ul>
                 <ul>
                     {sizes.map((size, index) => (
-                        <li onClick={() => setActiveSize(index)} className={activeSize === index ? "active" : ""} key={index}>{size}</li>
+                        <li
+                            onClick={() => setActiveSize(index)}
+                            className={activeSize === index ? 'active' : ''}
+                            key={index}
+                        >
+                            {size}
+                        </li>
                     ))}
                 </ul>
             </div>
@@ -52,7 +68,8 @@ const ItemBlock = ({ id ,title, price, imageUrl, sizes, types }) => {
                         height="12"
                         viewBox="0 0 12 12"
                         fill="none"
-                        xmlns="http://www.w3.org/2000/svg">
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
                         <path
                             d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
                             fill="white"
