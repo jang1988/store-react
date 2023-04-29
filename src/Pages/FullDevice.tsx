a1949ed15af5e4120c7840a8c7ae1aaeeb9f55fd
Baklay Petro <pustota1988@gmail.com>
2023-04-29 17:48:44 +0300

    fixErrorTYPESCRIPT

--- src/Pages/FullDevice.tsx
+++ src/Pages/FullDevice.tsx
@@ -1,16 +1,22 @@
-import axios from 'axios';
 import React from 'react';
+import axios from 'axios';
 import { useNavigate, useParams } from 'react-router-dom';
 
-const FullDevice = () => {
-    const navigate = useNavigate()
+const FullDevice: React.FC = () => {
+    const navigate = useNavigate();
 
     const { id } = useParams();
 
-    const [device, setDevice] = React.useState();
+    const [device, setDevice] = React.useState<{
+        imageUrl: string;
+        title: string;
+        rating: string;
+        sizes: Array<String>;
+        size: string;
+        price: number;
+    }>();
 
     React.useEffect(() => {
-
         (async function fetchDevice() {
             try {
                 const { data } = await axios.get(
@@ -19,15 +25,14 @@ const FullDevice = () => {
                 setDevice(data);
             } catch (error) {
                 console.log('error: ', error);
-                alert('Device not found')
-                navigate('/')
+                alert('Device not found');
+                navigate('/');
             }
-        })()
-
+        })();
     }, [id, navigate]);
 
     if (!device) {
-        return 'LOADING'
+        return <>'LOADING...'</>;
     }
 
     return (
@@ -35,7 +40,7 @@ const FullDevice = () => {
             <img src={device.imageUrl} alt="" />
             <h2>{device.title}</h2>
             <p>rating {device.rating}</p>
-            {device.sizes.map(size => size + ' ')}
+            {device.sizes.map((size) => size + ' ')}
             <h4>{device.price} grn</h4>
         </div>
     );
--- src/index.tsx
+++ src/index.tsx
@@ -9,12 +9,15 @@ import { store } from './redux/store';
 import { Provider } from 'react-redux';
 
 const rootElement = document.getElementById('root');
-const root = createRoot(rootElement);
 
-root.render(
-    <Provider store={store}>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </Provider>,
-);
+if (rootElement) {
+    const root = createRoot(rootElement);
+
+    root.render(
+        <Provider store={store}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </Provider>,
+    );
+}
